72775693d047b319b07425637c9f17a572397dc2
anipakerimbekova <ani.kerimbekova@mail.ru>
2021-07-06 16:55:28 +0600

    Initial commit

---
+++ app/src/main/java/com/geektech/android1_hw1_34/MainActivity.java
@@ -0,0 +1,17 @@
+package com.geektech.android1_hw1_34;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //-Сверстать страницу комбинируя несколько контейнеров(Constraint, Linear)
+        //-Вывести заголовки в файл strings
+        //-Иконки можете любые скачать и вставить
+    }
+}
